61a46040473a4d9adb127910869063a7a0b5c53c
khalilholberton <1071@holbertonschool.com>
2020-04-22 22:47:01 +0000

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""Flask web application.
-Host: 0.0.0.0
-port: 5000
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
+""" Python script that starts a flask application on 0.0.0.0 port 5000"""
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'
-
-    Arguments:
-        None
-
-    Returns:
-        (str)
-    """
+@app.route('/')
+def hello_world():
+    """print Hello HBNB"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
