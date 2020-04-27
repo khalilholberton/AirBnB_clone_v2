2e7310c084db24e3b46dc6bd14ca661c57b56a57
khalilholberton <1071@holbertonschool.com>
2020-04-27 01:45:03 +0000

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''script start a flask application on 0.0.0.0 port 5000'''
+'''script start flask application to listen
+ on 0.0.0.0 port 5000 with variables'''
+
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -23,7 +25,7 @@ def c(var):
     return "C " + var.replace('_', ' ')
 
 @app.route('/python')
-@app.route('/python/<var>')
+@app.route('/python/<text>')
 def dislay_python(text="is cool"):
     """
     print python with variable as given text and replaces _ with space
