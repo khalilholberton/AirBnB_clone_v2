59327e87d8cefe0752e8cea1a70437615b6a391a
khalilholberton <1071@holbertonschool.com>
2020-04-27 01:31:26 +0000

    fix

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''script start a flask application on 0.0.0.0 port 5000'''
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """print Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    """print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<var>')
+def c(var):
+    """print C with variable as given text"""
+    return "C " + var.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
