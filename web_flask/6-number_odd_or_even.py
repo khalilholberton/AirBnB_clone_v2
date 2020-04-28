debe804a6f4d4ef100e127b40f89fcc2def2ed74
khalilholberton <1071@holbertonschool.com>
2020-04-28 00:11:13 +0000

    fix

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+'''script start flask application to listen
+ on 0.0.0.0 port 5000 with variables'''
+
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """print Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    """print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<var>')
+def c(var):
+    """print C with variable as given text"""
+    return "C " + var.replace('_', ' ')
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def dislay_python(text="is cool"):
+    """
+    print python with variable as given text and replaces _ with space
+    """
+    return("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>')
+def display_num(n):
+    """
+    print n  only if int
+    """
+    return ("{:d} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def display_template_with_num(n):
+    """
+    display template with n integer
+    """
+    return (render_template('5-number.html', n=n))
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    """
+    display template with n integer n is even|odd” inside the tag BODY
+    """
+    return (render_template('6-number_odd_or_even.html', n=n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
